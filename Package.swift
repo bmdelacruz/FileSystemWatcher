89b7a8e2b64218bd489a254700c4e69cd63fc306
Bryan Dela Cruz <bdshi13@gmail.com>
2017-04-04 02:38:21 +0800

    updated dependency url and version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "inotify-swift-lib",
     dependencies: [
-      .Package(url: "../", majorVersion: 0)
+      .Package(url: "https://github.com/bmdelacruz/inotify-swift.git", majorVersion: 1)
     ]
 )
